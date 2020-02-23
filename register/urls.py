e1fef73ec407ac5dde6ec7ff0ff224947b3616f6
andrew <andrewnagynemr@gmail.com>
2020-02-23 11:56:43 +0200

    adding login and logout

--- register/urls.py
+++ register/urls.py
@@ -1,6 +1,10 @@
-from register import views
 from django.urls import path
+from register import views as register_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('form/', views.register)
+    path('register/', register_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='register/LoginFrom.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='register/LogOUT.html'), name='logout'),
+
 ]
