b7d8b44273a76df9e53b28849e193ef8194f6f9c
andrew <andrewnagynemr@gmail.com>
2020-02-23 23:17:39 +0200

    adding login feature

--- register/form.py
+++ register/form.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 
 class RegisterForm(UserCreationForm):
@@ -11,3 +12,9 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Email exists")
+        return self.cleaned_data
