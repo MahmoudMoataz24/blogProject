2364f7d15844c54978d47fec18f0a87e05f32a49
Sara Yasser <Sarayasser@users.noreply.github.com>
2020-02-24 10:39:33 +0200

    conflict resolved

--- admin_page/views.py
+++ admin_page/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
 from blog.models import *
 from django.http import HttpResponse,HttpResponseRedirect
-<<<<<<< HEAD
 from admin_page.forms import UserForm,PostForm,CategoryForm
-=======
-from admin_page.forms import UserForm
->>>>>>> develop
 # Create your views here.
 def viewAll(request):
 	user=User.objects.all()
@@ -23,7 +19,6 @@ def addUser(request):
 		user_form =UserForm() 
 		context = {'user_form':user_form}
 		return render(request,'admin_page/st_add.html',context)
-<<<<<<< HEAD
 
 def EditUser(request,num):
 	user=User.objects.get(id = num)
@@ -111,23 +106,4 @@ def catAdd(request):
 			'cat_form':cat_form,
 			'title':'Add'
 			}
-		return render(request,"/admin_page/cat_add.html",context)
-=======
-
-def EditUser(request,num):
-	user=User.objects.get(id = num)
-	if request.method=="POST":
-		user_form=UserForm(request.POST,instance=user)
-		if user_form.is_valid():
-			user_form.save()
-		return HttpResponseRedirect("/admin_page/all")
-	else:
-		user_form=UserForm(instance=user)
-		context={'user_form':user_form}
-		return render(request,'admin_page/st_add.html',context)
-
-def deleteUser(request,num):
-	user = User.objects.get(id = num)
-	user.delete()
-	return HttpResponseRedirect('/admin_page/all')
->>>>>>> develop
+		return render(request,"/admin_page/cat_add.html",context
