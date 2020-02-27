ebdd607083cfcae6fe6cf6a99ead7fba559f8df7
mahmoudmoataz24 <mahmoud.moatazz24@gmail.com>
2020-02-27 12:29:45 +0200

    show all posts method added to posts application view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponseRedirect
+from posts.models import Post, Comments, reply, Category, Likes
 # Create your views here.
+
+
+def index(request):
+    postList = Post.objects.all().order_by('created')
+    postDic = {'posts': postList}
+    return render(request, 'posts/post.html', context=postDic)
