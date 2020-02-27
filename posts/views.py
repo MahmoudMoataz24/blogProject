14f9d0c01ef11f001619cf083457336f22d0561b
mahmoudmoataz24 <mahmoud.moatazz24@gmail.com>
2020-02-27 12:34:21 +0200

    show single post functionality added

--- posts/views.py
+++ posts/views.py
@@ -7,3 +7,22 @@ def index(request):
     postList = Post.objects.all().order_by('created')
     postDic = {'posts': postList}
     return render(request, 'posts/post.html', context=postDic)
+
+
+def showSinglePost(request, postID):
+    post = Post.objects.get(id=postID)
+    comments = Comments.objects.filter(postID_id=postID)
+    cats = Category.objects.all()
+
+    data = []
+    for comment in comments:
+        try:
+            reply = Reply.objects.get(comId_id=comment.id)
+            dic = {'comm': comment, 'rep': reply}
+        except Exception as e:
+            dic = {'comm': comment}
+        finally:
+            data.append(dic)
+    context = {'post': post, 'data': data,
+               'cats': cats, 'likeCount': likeCount}
+    return render(request, 'posts/single.html', context)
